d2442641a16c148a0fb410d7c1c3dcd10bdecb9a
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-06-16 11:44:32 +0530

    added basic node setup

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Welcome to the world of NodeJS")
+
+var message = "Hello World, you there ?"
+
+function display(message){
+    return message
+}
+
+console.log(display(message))
+
+var array =["Scott", "Adam", "Tuan","Uma"]
+console.log(array)
